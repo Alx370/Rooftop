d2387ad701ae86c0d2d95395241e304fb9cc78e5
Gualtiero <gualtiero.begalla@edu-its.it>
2025-11-09 16:05:27 +0100

    Inizializzazione pagina HTML e CSS

--- capibaraweb-main/src/pages/ChiSiamo/chisiamo.jsx
+++ capibaraweb-main/src/pages/ChiSiamo/chisiamo.jsx
@@ -1,5 +1,31 @@
+import "./chisiamo.css";
+
 const ChiSiamo = () => {
-  return <h1 className="text-2xl font-semibold"> Chi Siamo</h1>;
+  return (
+    <div className="chisiamo">
+
+      <section className="hero">
+        <div className="hero-content">
+
+          <div className="hero-text">
+            <h1>Chi siamo</h1>
+            <p><strong>Immobiliaris</strong> è l'agenzia immobiliare di riferimento a <strong>Torino</strong>, specializzata nella vendita di case con <strong>consulenza su misura</strong> e <strong>profonda conoscenza</strong> del territorio.</p>
+          </div>
+
+          <div className="hero-image">
+            <img src="src\pages\Home\img\PalazzoModerno.png" alt="Palazzo moderno" />
+          </div>
+
+        </div>
+      </section>
+
+      <section className="hero-title">
+        <h1>Scopri tutti i nostri agenti</h1>
+        <p><strong>Immobiliaris</strong> è l'agenzia immobiliare di riferimento a <strong>Torino</strong>, specializzata nella vendita di case con <strong>consulenza su misura</strong> e <strong>profonda conoscenza</strong> del territorio.</p>
+      </section>
+
+    </div>
+  );
 };
 
 export default ChiSiamo;

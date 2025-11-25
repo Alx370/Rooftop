1d12d70154f1f0b8dc794f1a6f6baa25fb0bebb1
andre <andreaalume66@gmail.com>
2025-11-25 20:38:53 +0100

    Add ChatRequest model class for handling chat messages

---
+++ Progetto_Rooftop/src/main/java/Immobiliaris/Progetto_Rooftop/Model/ChatRequest.java
@@ -0,0 +1,15 @@
+package Immobiliaris.Progetto_Rooftop.Model;
+
+public class ChatRequest {
+    private String message;
+    
+    public ChatRequest() {}
+    
+    public String getMessage() {
+        return message;
+    }
+    
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

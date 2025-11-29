af1ad4b650a1aa1e4f6540c6f1ed5f84b251c948
andre <andreaalume66@gmail.com>
2025-11-29 13:38:21 +0100

    Add chat streaming endpoint to ChatController

--- Progetto_Rooftop/src/main/java/Immobiliaris/Progetto_Rooftop/Controller/ChatController.java
+++ Progetto_Rooftop/src/main/java/Immobiliaris/Progetto_Rooftop/Controller/ChatController.java
@@ -7,6 +7,7 @@ import Immobiliaris.Progetto_Rooftop.Services.OllamaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Controller
 public class ChatController {
@@ -26,4 +27,31 @@ public class ChatController {
         String response = ollamaService.chat(request.getMessage());
         return new ChatResponse(response);
     }
+
+    // Endpoint REST to stream chat messages
+    @PostMapping("/api/chat/stream")
+    @ResponseBody
+    public SseEmitter chatStream(@RequestBody ChatRequest request) {
+
+        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+
+        new Thread(() -> {
+            try {
+                ollamaService.chatStream(request.getMessage(), token -> {
+                    try {
+                        emitter.send(SseEmitter.event().name("message").data(token));
+                    } catch (Exception e) {
+                        emitter.completeWithError(e);
+                    }
+                });
+
+                emitter.complete();
+            } catch (Exception e) {
+                emitter.completeWithError(e);
+            }
+        }).start();
+
+        return emitter;
+    }
+
 }

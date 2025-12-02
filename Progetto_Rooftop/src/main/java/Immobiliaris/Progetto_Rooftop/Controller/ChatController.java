fa8d1a1ddedd073c823dfbda09b3281b85eca144
andre <andreaalume66@gmail.com>
2025-12-02 22:30:17 +0100

    Removed endpoint chat stream

--- Progetto_Rooftop/src/main/java/Immobiliaris/Progetto_Rooftop/Controller/ChatController.java
+++ Progetto_Rooftop/src/main/java/Immobiliaris/Progetto_Rooftop/Controller/ChatController.java
@@ -5,11 +5,9 @@ import Immobiliaris.Progetto_Rooftop.Model.ChatResponse;
 import Immobiliaris.Progetto_Rooftop.Services.OllamaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-@Controller
+@RestController
 public class ChatController {
     
     @Autowired
@@ -26,32 +24,6 @@ public class ChatController {
     public ChatResponse chat(@RequestBody ChatRequest request) {
         String response = ollamaService.chat(request.getMessage());
         return new ChatResponse(response);
-    }
-
-    // Endpoint REST to stream chat messages. (Testing)
-    @PostMapping("/api/chat/stream")
-    @ResponseBody
-    public SseEmitter chatStream(@RequestBody ChatRequest request) {
-
-        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
-
-        new Thread(() -> {
-            try {
-                ollamaService.chatStream(request.getMessage(), token -> {
-                    try {
-                        emitter.send(SseEmitter.event().name("message").data(token));
-                    } catch (Exception e) {
-                        emitter.completeWithError(e);
-                    }
-                });
-
-                emitter.complete();
-            } catch (Exception e) {
-                emitter.completeWithError(e);
-            }
-        }).start();
-
-        return emitter;
     }
 
 }
